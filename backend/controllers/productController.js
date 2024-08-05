4f3fd8b7ebca70887a06f2bc0234acc39124965f
Tim Hsiung <timhsiung@Tims-MacBook-Pro.local>
2024-08-05 11:26:23 -0700

    backend added cart features add + get + order + remove

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,20 +1,17 @@
-const Product = require("../models/productModel.js");
+const Product = require('../models/productModel.js');
 
 let productController = {};
 
 productController.getProducts = async (req, res, next) => {
-  
   try {
     const products = await Product.find({});
     res.json(products);
   } catch (err) {
     return next({
-      message: "error in getProducts: " + err,
+      message: 'error in getProducts: ' + err,
       log: err,
     });
   }
 };
-
-
 
 module.exports = productController;
